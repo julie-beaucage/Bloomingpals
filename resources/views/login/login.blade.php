b6d48d88a2eaf85c4ec4005351821cd7da2f0a25
julie-beaucage <201386196@edu.clg.qc.ca>
2024-09-11 13:59:54 -0400

    Model et Controller

---
+++ resources/views/login/login.blade.php
@@ -0,0 +1,33 @@
+@extends("master")
+
+@section("content")
+<link rel="stylesheet" href="{{ asset('./css/login.css') }}">
+    <div class="formContainerLogin">
+        <header>
+            <h2>Se connecter</h2>
+            <hr>
+        </header>
+        <form action="/login" method="POST">
+            @csrf
+            <div class="inputContainer">
+                <div class="entryarea entryarealogin">
+                    <input type="text" id="email"  placeholder="courriel" name="email" value="{{ old('email') }}" required/>
+                </div>
+            </div>
+            <div class="inputContainer">
+                <div class="entryarea entryarealogin">
+                    <input type="password" id="password" class="alpha" name="password" value="{{ old('password') }}" required/>
+                    <label for="password">Mot de passe</label>
+                </div>
+                <input type='hidden' class="errorMessage" name='error'></input>
+                    @error('email')
+                        <p class="errorMessage">{{ $message }}</p>
+                    @enderror
+            </div>
+            <div class="formButton">
+                <button type="submit" id="submitBtn" class="btn">Accéder</button>
+            </div>
+            <p>Vous n'avez pas de compte? <a href="/register">S'inscrire</a></p>
+        </form>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('home.feed');
+});
+*/
+
+Route::get('/', function () {
+    return view('login.login');
 });
 
 Route::get('/home', function () {
