42c519a213c20a89c0ed0b3126e797c403dfedc9
julie-beaucage <201386196@edu.clg.qc.ca>
2024-09-11 17:23:31 -0400

    css login
    d

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     <div class="formContainerLogin">
         <header>
-           <h1>BloomingPals</h1>
+           <h1>BloomingPals </h1>
             <h2>Se connecter</h2>
             <hr>
         </header>
