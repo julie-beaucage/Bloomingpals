67093b937bed1fc1d585d268ca8f4b735278fa32
julie-beaucage <201386196@edu.clg.qc.ca>
2024-09-25 16:54:46 -0400

    update

--- resources/views/bundles/stylesBundle.blade.php
+++ resources/views/bundles/stylesBundle.blade.php
@@ -2,10 +2,6 @@
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
-<!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- Bootstrap JS -->
-
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 <!----------------------------------------------------------------------------------------->
