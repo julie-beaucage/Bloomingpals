575427aa96fbc139762c1d4548c9fcfab2293dde
Alexis Brosseau <alexisbrosseau@outlook.com>
2024-09-30 21:13:01 -0400

    sql fix

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -1,5 +1,4 @@
-
-DROP DATABASE IF EXISTS `BloomingPals`;
+-- DROP DATABASE IF EXISTS `BloomingPals`;
 Create DATABASE IF NOT EXISTS `BloomingPals`  DEFAULT CHARACTER SET utf8mb4;
 USE BloomingPals;
 
@@ -47,8 +46,8 @@ CREATE TABLE IF NOT EXISTS utilisateur (
     background_image VARCHAR(255), 
     genre ENUM('homme', 'femme', 'non-genre') NOT NULL,
     password CHAR(128) NOT NULL,
-    email_verified_at TIMESTAMP NULL DEFAULT NULL, 
-    update_at TIMESTAMP NULL DEFAULT NULL, 
+    email_verified_at TIMESTAMP NULL DEFAULT NULL,
+    updated_at TIMESTAMP NULL DEFAULT NULL,
     remember_token VARCHAR(100) NULL,
     FOREIGN KEY (type_personnalite) REFERENCES type_personnalite (id)
 ) ENGINE=InnoDB;
