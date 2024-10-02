bcc6ec54a3b6cad9d5a5c307739e6c7d7f2bb198
Alexis Brosseau <alexisbrosseau@outlook.com>
2024-10-02 12:16:20 -0400

    rollback interets

--- resources/views/interets/interets.blade.php
+++ resources/views/interets/interets.blade.php
@@ -29,15 +29,46 @@
             @endforeach
 
             @if (empty($interetsPourCategorie))
-                <p>Aucun intérêt dans cette catégorie {{ $categorie->nom}}.</p>
+                <p>Aucun intérêt dans la catégorie {{ $categorie->nom }}.</p>
             @else
                 @foreach ($interetsPourCategorie as $interetUtilisateur)
-                <div class="tag interet-{{ strtolower($categorie->nomCategorie) }}">{{ $interetUtilisateur->nom }}</div>
+                    <div class="tag">{{ $interetUtilisateur->nom }}</div>
                 @endforeach
             @endif
         </div>
     @endforeach
-@include ('interets/interetEdit')
-</body>
 
+    <div class="interet-overlay" id="overlay" style="display: none;">
+        <div class="interet-modal">
+            <h3 class="titreModalInteret">Modifier vos intérêts :</h3>
+            <button class="close-button"
+                onclick="window.location.href='{{ route('profile', Auth::user()->id) }}'">&times;</button>
+            <form action="{{ route('interets.update_Interets') }}" method="POST" id="interetForm">
+                @csrf
+                @method('PUT')
+
+                @if (!empty($categories))
+                    @foreach ($categories as $categorie)
+                        <div class="interet-categorie-div">
+                            <h3>{{ $categorie->nom }}</h3>
+                            @foreach ($interets as $interet)
+                                @if ($interet->id_categorie == $categorie->id)
+                                    <div class="interet-tag {{ in_array($interet->id, $interetsUtilisateurTab) ? 'interet-selected' : '' }}" 
+                                        data-id="{{ $interet->id }}">
+                                        {{ $interet->nom }}
+                                    </div>
+                                @endif
+                            @endforeach
+                        </div>
+                    @endforeach
+                @else
+                    <p>Aucune catégorie trouvée.</p>
+                @endif
+                <input type="hidden" name="interets" id="interetSelectedInterets" value="{{ implode(',', $interetsUtilisateurTab) }}"> 
+
+                <button type="submit" class="interet-btn-primary">Sauvegarder les changements</button>
+            </form>
+        </div>
+    </div>
+</body>
 </html>
