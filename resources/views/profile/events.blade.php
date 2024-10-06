56857f30f634537c5e4d6afed4705f8e0416e237
William-Bégin <energie1000@outlook.com>
2024-10-05 20:14:25 -0400

    event done

--- resources/views/profile/events.blade.php
+++ resources/views/profile/events.blade.php
@@ -1,6 +1,5 @@
 <?php
-$eventsHtml = "";
-
+$eventsHtml = '<div class="events">';
 foreach ($eventsData as $eventData) {
     $eventImage = "";
     if ($eventData->image != null) {
@@ -9,16 +8,34 @@ foreach ($eventsData as $eventData) {
         $default_images=rand(1,3);
         $eventImage = '\images\meetup_default'.$default_images.'.png'; 
     }
+
+
     $eventsHtml .= <<<HTML
-        <div class="event">
-            <img class="image" src="{{$eventImage}}">
-            $eventData->id<br>
-            <div>
-                $eventData->date
+        <a href="">
+            <div class="event">
+                <img class="image" src="$eventImage">
+                <div class="title3 darkgreyText">
+                    $eventData->nom
+                </div>
+                <div class="hr">
+                </div>
+                <div class="flexSplitH">
+                    <div>
+
+                        Date <br>
+                        $eventData->date
+                    </div>
+                    <div>
+
+                        Lieu <br>
+                        $eventData->ville
+                    </div>
+                </div>
             </div>
-        </div>
+        </a>
     HTML;
 }
+$eventsHtml .= '</div>';
 
 echo $eventsHtml;
 
