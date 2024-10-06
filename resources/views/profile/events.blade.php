87118f579d2a8d3d5263cfe83e5e3a20bdfda8e2
William-Bégin <energie1000@outlook.com>
2024-10-05 20:26:13 -0400

    event finish but ugly

--- resources/views/profile/events.blade.php
+++ resources/views/profile/events.blade.php
@@ -8,27 +8,35 @@ foreach ($eventsData as $eventData) {
         $default_images=rand(1,3);
         $eventImage = '\images\meetup_default'.$default_images.'.png'; 
     }
-
+    $route = route("event", ["id" => $eventData->id]);
 
     $eventsHtml .= <<<HTML
-        <a href="">
+        <a href="$route">
             <div class="event">
                 <img class="image" src="$eventImage">
-                <div class="title3 darkgreyText">
-                    $eventData->nom
-                </div>
-                <div class="hr">
-                </div>
-                <div class="flexSplitH">
-                    <div>
-
-                        Date <br>
-                        $eventData->date
+                <div class="eventInfoContainer">
+                    <div class="title3 darkgreyText">
+                        $eventData->nom
                     </div>
-                    <div>
-
-                        Lieu <br>
-                        $eventData->ville
+                    <div class="hr">
+                    </div>
+                    <div class="flexSplitH">
+                        <div>
+                            <div class="darkgreyText">
+                                Date
+                            </div>
+                            <div class="greyText">
+                                $eventData->date
+                            </div>
+                        </div>
+                        <div>
+                            <div class="darkgreyText">
+                                Lieu
+                            </div>
+                            <div class="greyText">
+                                $eventData->ville
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
