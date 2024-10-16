bc1c46bc67a1f29d4184f1a0eb746335aa26f5aa
William-Bégin <energie1000@outlook.com>
2024-10-16 10:55:18 -0400

    push

--- resources/views/partial_views/user_cards.blade.php
+++ resources/views/partial_views/user_cards.blade.php
@@ -7,10 +7,50 @@ if (count($users)  == 0) {
     return;
 }
 
+$html = <<<HTML
+    <div class="userGrid">
+HTML;
+
 foreach ($users as $user) {
-    echo <<< HTML
+
+
+    $buttonHtml = "";
+    $friendFound = false;
+    foreach ($amis as $ami) {
+        if ($ami->id == $user->id) {
+            $buttonHtml = `<div class="lastSpacing" value="addded">Ami</div>`;
+            $friendFound = true;
+        }
+    }
+
+    if (!$friendFound) { 
+        $buttonHtml = `<div class="lastSpacing button" value="add"><a class="blue_button" href="">Ajouter</a></div>`;
+    }
+
+    $imageUser = $user->image_profil ? asset('storage/' . $user->image_profil) : asset('/images/simple_flower.png');
+    $html .= <<<HTML
         <div>
-            <span>{$user->first_name}</span>
+            <div class="profile_icon no_select" style="background-image: url($imageUser)">
+                        
+            </div>
+            <div>{$user->first_name}</div>
+            <div>{$user->last_name}</div>
+            $buttonHtml
         </div>
     HTML;
 }
+
+$html .= <<< HTML
+    </div>
+HTML;
+
+echo $html;
+
+?>
+
+<script>
+    $(".lastButtonSpacing").on("click", function(button) {
+        button
+    });
+
+</script>
