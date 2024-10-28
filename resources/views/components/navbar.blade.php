75be54b16fe9ea4ecc97a190cb84ee55e2bbbf63
Alexis Brosseau <alexisbrosseau@outlook.com>
2024-10-27 20:43:22 -0400

    fix_navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -44,7 +44,7 @@
                 $class = $id == Route::current()->uri() ? $class . ' active' : $class;
 
                 if ($id === 'profile') {
-                    echo <<<HTML
+                    echo <<< HTML
                                 <a class="$class" href="$url">
                                     <div class="shrinked_title shrinked_only">
                                         <span class="title">$title</span>
@@ -56,7 +56,7 @@
                                 </a>
                             HTML;
                 } else {
-                    echo <<<HTML
+                    echo <<< HTML
                                 <a class="$class" href="$url">
                                     <div class="shrinked_title shrinked_only">
                                         <span class="title">$title</span>
@@ -69,17 +69,6 @@
                             HTML;
                 }
             }
-
-            echo <<<HTML
-                        <svg style="visibility: hidden; position: absolute;" width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1">
-                            <defs>
-                                <filter id="goo"><feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />    
-                                    <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo" />
-                                    <feComposite in="SourceGraphic" in2="goo" operator="atop"/>
-                                </filter>
-                            </defs>
-                        </svg>
-                    HTML;
         @endphp
     </nav>
 @endif
