94722ea2659a9bd50dc040908825be298db8b0f7
julie-beaucage <201386196@edu.clg.qc.ca>
2024-10-28 20:44:11 -0400

    ok fini

--- resources/views/partial_views/user_cards.blade.php
+++ resources/views/partial_views/user_cards.blade.php
@@ -3,12 +3,16 @@ use App\Models\User_Interest;
 use App\Models\Interest;
 use App\Models\Category_Interest;
 
-if (count($users)  == 0) {
+$currentUser = Auth::user();
+
+if (count($users) == 0) {
     echo '';
     return;
 }
 
 foreach ($users as $user) {
+    $userPersonality = $user->getPersonalityType(); 
+    $affinityPercentage = $currentUser->calculateAffinity($user->id, $currentUser->id);
     $image = $user->image_profil ? asset('storage/' . $user->image_profil) : asset('/images/simple_flower.png');
     $tags = "";
 
@@ -18,32 +22,27 @@ foreach ($users as $user) {
     for ($i = 0; $i < $count && $i < 2; $i++) {
         $interest = Interest::find($user_interests[$i]->id_interest);
         if ($interest == null) continue;
-        
         $tags .= '<span class="tag" style="background-color: var(--category-'. $interest->id_category .')">' . $interest->name . '</span>';
     }
 
     if ($count > 2) {
-        $tags .= '<span class="tag square_tag">+' . $count - 2 . '</span>';
+        $tags .= '<span class="tag square_tag">+' . ($count - 2) . '</span>'; 
     }
-
-    $affinity = round(Auth::user()->affinity($user_interests->pluck('id_interest')) * 100);
-
+    
     echo <<< HTML
-        <a class="card_long no_select hover_darker" href="profile/$user->id">
-            <div class="banner">
+        <a class="card_long no_select hover_darker $userPersonality" href="profile/$user->id">
+            <div class="banner {$userPersonality}">
                 <img src="$image" alt="Image de profile de $user->first_name $user->last_name">
             </div>
             <div class="content">
                 <div class="header">
-                    <div class="text_nowrap name_cntr">
+                    <div class="text_nowrap">
                         <span class="name">$user->first_name $user->last_name</span>
                     </div>
-                    <div class="tags_cntr">
-                        {$tags}
-                    </div>
+                    $tags
                 </div>
                 <div class="infos">
-                    <span>$affinity% d'affinité avec vous</span>
+                    <span>$affinityPercentage d'affinité avec vous</span>
                 </div>
             </div>
         </a>
