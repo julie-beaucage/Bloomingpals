6232c05bd41e7e0093fc5f8da487f7a23be738b4
William-Bégin <energie1000@outlook.com>
2024-10-29 20:20:09 -0400

    update from main correction

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -56,18 +56,28 @@
     <div class="containerOnglerMain">
         <div class="listOnglet">
             <ul class="nav nav-tabs justify-content-center">
-                <li class="nav-item">
-                    <a class="nav-link tab-link {{ request()->is('interets/*/interets') || request()->is('profile/personnalite') || !request()->is('profile/*') ? 'active' : '' }}"
-                        href="{{ route('interets.interets', $user->id) }}" data-target="interets/interests">Intérêts</a>
+                <li class="nav-item" title="Intérêts">
+                    <a class="nav-link tab-link {{ request()->is('profile/informations') ? 'active' : '' }}"
+                        href="{{ route('profile.informations', $user->id) }}" data-target="profile/informations">Informations</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item" title="Amis">
                     <a class="nav-link tab-link {{ request()->is('profile/amis') ? 'active' : '' }}"
                         href="{{ route('profile.amis', $user->id) }}" data-target="profile/amis">Amis</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item" title="Personalité">
                     <a class="nav-link tab-link {{ request()->is('profile/personnalite') ? 'active' : '' }}"
                         href="{{ route('profile.personnalite', $user->id) }}"
                         data-target="profile/personnalite">Personnalité</a>
+                </li>
+                <li class="nav-item" title="Events">
+                    <a class="nav-link tab-link {{ request()->is('profile/events') ? 'active' : '' }}"
+                        href="{{ route('profile.events', $user->id) }}"
+                        data-target="profile/events">Événement</a>
+                </li>
+                <li class="nav-item" title="Rencontres">
+                    <a class="nav-link tab-link {{ request()->is('profile/rencontres') ? 'active' : '' }}"
+                        href="{{ route('profile.rencontres', $user->id) }}"
+                        data-target="profile/rencontres">Rencontres</a>
                 </li>
             </ul>
         </div>
