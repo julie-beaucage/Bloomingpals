212f62c7f50770c55e77b21c06f3f2e485d28ecd
William-Bégin <95308274+electrix1337@users.noreply.github.com>
2024-11-06 15:03:12 -0500

    vvcxz

--- resources/views/admin/adminReports.blade.php
+++ resources/views/admin/adminReports.blade.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="{{ asset('css/overlay-modal.css') }}">
     <link rel="stylesheet" href="{{ asset('css/interets.css') }}">
     <link rel="stylesheet" href="{{ asset('css/personality.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/reports.css') }}">
 @endsection()
 
 @section('content')
@@ -15,93 +16,48 @@
     $html = "";
     use App\Models\User_Interest; 
     use App\Models\Interest;
-
-
+    
     foreach ($reports as $report) {
 
 
         $image = $report['user_send']->image_profil ? asset('storage/' . $report->image_profil) : asset('/images/simple_flower.png');
-        $tags = "";
-
-        $user_interests = User_Interest::where('id_user', $report['user_send']->id)->get();
-        $count = "";
-        if ($count != "Undefined") {
-            $count = count($user_interests);
-        } else {
-            $count = 0;
-        }
-        
-        for ($i = 0; $i < $count && $i < 2; $i++) {
-            $interest = Interest::find($user_interests[$i]->id_interest);
-            if ($interest == null) continue;
-            
-            $tags .= '<span class="tag" style="background-color: var(--category-'. $interest->id_category .')">' . $interest->name . '</span>';
-        }
-
-        if ($count > 2) {
-            $tags .= '<span class="tag square_tag">+' . ($count - 2) . '</span>';
-        }
-
         $route = route("profile", ["id" => $report['user_send']->id]);
 
-        $html .= <<<HTML
-            <a class="card_long no_select hover_darker" href="$route">
-                <div class="banner">
-                    <img src="$image" alt="Image de profile de {$report['user_send']->first_name} {$report['user_send']->last_name}">
-                </div>
-                <div class="content">
-                    <div class="header">
-                        <div class="text_nowrap name_cntr">
-                            <span class="name">{$report['user_send']->first_name} {$report['user_send']->last_name}</span>
-                        </div>
-                        <div class="tags_cntr">
-                            {$tags}
-                        </div>
-                    </div>
-                </div>
-            </a>
-        HTML;
-
         $image = $report['user_receive']->image_profil ? asset('storage/' . $report->image_profil) : asset('/images/simple_flower.png');
-        $tags = "";
-
-        $user_interests = User_Interest::where('id_user', $report['user_receive']->id)->get();
-        $count = "";
-        if ($count != "Undefined") {
-            $count = count($user_interests);
-        } else {
-            $count = 0;
-        }
-        
-        for ($i = 0; $i < $count && $i < 2; $i++) {
-            $interest = Interest::find($user_interests[$i]->id_interest);
-            if ($interest == null) continue;
-            
-            $tags .= '<span class="tag" style="background-color: var(--category-'. $interest->id_category .')">' . $interest->name . '</span>';
-        }
-
-        if ($count > 2) {
-            $tags .= '<span class="tag square_tag">+' . ($count - 2) . '</span>';
-        }
-
         $route = route("profile", ["id" => $report['user_receive']->id]);
 
         $html .= <<<HTML
-            <a class="card_long no_select hover_darker" href="$route">
-                <div class="banner">
-                    <img src="$image" alt="Image de profile de {$report['user_receive']->first_name} {$report['user_receive']->last_name}">
-                </div>
-                <div class="content">
-                    <div class="header">
-                        <div class="text_nowrap name_cntr">
-                            <span class="name">{$report['user_receive']->first_name} {$report['user_receive']->last_name}</span>
+            <div class="flexRow maxWidth">
+                    <a class="userCard card_long no_select hover_darker" href="$route">
+                        <div class="banner">
+                            <img src="$image" alt="Image de profile de {$report['user_send']->first_name} {$report['user_send']->last_name}">
                         </div>
-                        <div class="tags_cntr">
-                            {$tags}
+                        <div class="content">
+                            <div class="header">
+                                <div class="text_nowrap name_cntr">
+                                    <span class="name">{$report['user_send']->first_name} {$report['user_send']->last_name}</span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </a><br><br>
+                    </a>
+                        <div class="whitebg flexRow center">
+                            <div>
+                                report to:
+                            </div>
+                        </div>
+                    <a class="userCard card_long no_select hover_darker" href="$route">
+                        <div class="banner">
+                            <img src="$image" alt="Image de profile de {$report['user_receive']->first_name} {$report['user_receive']->last_name}">
+                        </div>
+                        <div class="content">
+                            <div class="header">
+                                <div class="text_nowrap name_cntr">
+                                    <span class="name">{$report['user_receive']->first_name} {$report['user_receive']->last_name}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </a><br><br>
+            </div>
         HTML;
     }
 
