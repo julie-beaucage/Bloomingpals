79eaeb09bf943fae38bf21d0395fb4c830bb7135
julie-beaucage <201386196@edu.clg.qc.ca>
2024-11-10 16:23:40 -0500

    terminer

--- app/Http/Controllers/InterestsController.php
+++ app/Http/Controllers/InterestsController.php
@@ -15,7 +15,6 @@ class InterestsController extends Controller
 {
     public function interets($id)
     {
-        Log::info("1ttttttttttttttttttttttttttttttttttttts");
         $user = User::find($id);
         $categories = Category_Interest::all(); 
         $interets = Interest::all(); 
@@ -27,7 +26,6 @@ class InterestsController extends Controller
     public function update_Interets(Request $request)
     {
         $selectedInterets = $request->input('interets');
-    
         DB::statement("CALL add_user_interests(?, ?)", [Auth::user()->id, $selectedInterets]);
         return redirect()->back()->with('success', 'Vos intérêts ont été mis à jour avec succès.');
     }
--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -155,7 +155,9 @@
 
         @section('script')
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        
         <script src="{{ asset('/js/profileOnglet.js') }}"></script>
+        
         <script src="{{ asset('/js/resendEmail.js') }}"></script>
         <script>
             function Confirmm() {
