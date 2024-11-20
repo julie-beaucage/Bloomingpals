365c0b4ac021af7127f9144652c299a62ac2870f
julie-beaucage <201386196@edu.clg.qc.ca>
2024-11-20 13:01:43 -0500

    ok

--- resources/views/pals/pals.blade.php
+++ resources/views/pals/pals.blade.php
@@ -2,7 +2,7 @@
 
 @section('style')
 <link rel="stylesheet" type="text/css" href="{{ asset('css/search.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('css/cards.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('css/palCard.css') }}">
 @endsection
 
 @section('content')
--- webpack.mix.js
+++ webpack.mix.js
@@ -31,6 +31,7 @@ mix.postCss('resources/css/overlay-modal.css', 'public/css', []);
 mix.postCss('resources/css/home.css', 'public/css', []);
 mix.postCss('resources/css/notifications.css', 'public/css', []);
 mix.postCss('resources/css/feed.css', 'public/css', []);
+mix.postCss('resources/css/feed.css', 'palCard/css', []);
 mix.sass('resources/scss/loading.scss', 'public/css', []);
 
 // For development
